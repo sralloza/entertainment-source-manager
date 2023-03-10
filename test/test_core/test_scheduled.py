578198b68925798d3b513bd83ddbcc8e8eed7d72
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2023-03-10 22:10:39 +0100

    add tests for scheduled core

--- test/test_core/test_scheduled.py
+++ test/test_core/test_scheduled.py
@@ -19,17 +19,18 @@ EPISODE_LIST = [ScheduledEpisode(**x) for x in loads(EPISODES_FILE.read_text())]
 
 @pytest.mark.asyncio
 @freeze_time("2018-01-01")
+@pytest.mark.parametrize("assume_new", [True, False])
 @mock.patch("app.core.scheduled.TodoistRepository")
-async def test_process_scheduled_episodes(todoist_repo_mock):
+async def test_process_scheduled_episodes(todoist_repo_mock, assume_new):
     repo_mock = mock.AsyncMock()
     todoist_repo_mock.return_value = repo_mock
     repo_mock.list_tasks.return_value = TASK_LIST
 
-    await process_scheduled_episodes(EPISODE_LIST)
+    await process_scheduled_episodes(EPISODE_LIST, assume_new)
 
     todoist_repo_mock.assert_called_once()
     repo = todoist_repo_mock.return_value
-    assert repo.list_tasks.call_count == 2
+    assert repo.list_tasks.call_count == (3 if assume_new else 2)
     repo.list_tasks.assert_any_call(project_id="project-1")
     repo.list_tasks.assert_any_call(project_id="project-2")
 
@@ -40,7 +41,10 @@ async def test_process_scheduled_episodes(todoist_repo_mock):
         section_id=None,
         due_date=date(2019, 1, 1),
     )
-    repo.create_task.assert_called_once_with(task_create)
+    if assume_new:
+        assert repo.create_task.call_count == 2
+    else:
+        repo.create_task.assert_called_once_with(task_create)
 
     task_update = TaskUpdate(
         description="Released: 2019-01-07 on nginx",
